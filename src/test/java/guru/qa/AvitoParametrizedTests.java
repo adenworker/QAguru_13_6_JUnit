2260fc45502b4b2b46276465257af19516fbfe73
Alexander Lisichkin <adenworker@gmail.com>
2022-06-12 12:11:07 +0300

    Cleaned up some garbage

--- src/test/java/guru/qa/AvitoParametrizedTests.java
+++ src/test/java/guru/qa/AvitoParametrizedTests.java
@@ -1,9 +1,11 @@
 package guru.qa;
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
@@ -13,7 +15,8 @@ import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static java.util.Arrays.asList;
 
 public class AvitoParametrizedTests {
@@ -22,7 +25,8 @@ public class AvitoParametrizedTests {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
     }
-/*-------------------------------------------------------------------------------------------------------------------*/
+
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /*Простой тест с именем*/
     @DisplayName("Простая проверка открытия \"Avito\"")
     @Test
@@ -31,7 +35,7 @@ public class AvitoParametrizedTests {
         $("[data-marker='header-navigation']").shouldHave(text("Недвижимость"), text("Работа"));
     }
 
-/*-------------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /*Итерационный тест с 2 параметрами, который запускается последовательно 2 раза. Данные хранятся тут же в ValueSource
     Проверяет на наличие на новой странице тот же текст, что мы вводим в поисковую строку.*/
     @ValueSource(strings = {
@@ -44,7 +48,7 @@ public class AvitoParametrizedTests {
         $("div [data-marker='item']").shouldHave(text(testDataValue)).shouldBe(visible);
     }
 
-/*-------------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /*Итерационный тест с 2 параметрами, который запускается последовательно 2 раза. Данные хранятся в специальном
     Enum классе в виде констант.
     Проверяет на наличие на новой странице независимую от запроса строку (1 кейс - 1 Enum элемент).*/
@@ -56,7 +60,7 @@ public class AvitoParametrizedTests {
         $("[data-marker='page-title/text']").shouldHave(text(enumData.result)).shouldBe(visible);
     }
 
-/*-------------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /*Итерационный тест с 2 параметрами, один из которых - список, который запускается последовательно необходимое
     количество раз (зависит от объема тестовых данных). Данные хранятся в Stream<Arguments> контейнере.
     Проверяет на наличие на новой странице 2 независимых строк (1 кейс - 1 список строк).*/
@@ -84,8 +88,8 @@ public class AvitoParametrizedTests {
     private boolean filterTextByExpectedResults(String actualText, List<String> expectedResults) {
         boolean isSuccessfulFiltering = true;
 
-        for (String expectedResult : expectedResults){
-            if (!actualText.contains(expectedResult)){
+        for (String expectedResult : expectedResults) {
+            if (!actualText.contains(expectedResult)) {
                 isSuccessfulFiltering = false;
                 break;
             }
@@ -93,7 +97,7 @@ public class AvitoParametrizedTests {
         return isSuccessfulFiltering;
     }
 
-/*-------------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /*Итерационный тест с 2 параметрами, который запускается последовательно необходимое количество раз (зависит от
     объема тестовых данных). Данные хранятся тут же в CsvSource контейнере
     Проверяет на наличие на новой странице второго текста из пары из тестовых данных (1 кейс - 1 пара).*/
@@ -108,7 +112,7 @@ public class AvitoParametrizedTests {
         $$("[data-marker='page-title/text']").find(text(expectedResult)).shouldBe(visible);
     }
 
-/*-------------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /*Итерационный тест с 2 параметрами, который запускается последовательно необходимое количество раз (зависит от
     объема тестовых данных). Данные хранятся в .csv файле в папке resources/test_data.
     Проверяет на наличие на новой странице второго текста из пары из тестовых данных (1 кейс - 1 пара).*/
